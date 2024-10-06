46b24e273580276bb0b4f21c5601c8d453891dee
Manu Vanneste <125195959+manulykebe@users.noreply.github.com>
2024-10-06 22:51:34 +0200

    Update index-serial.js

--- src/html/js/index-serial.js
+++ src/html/js/index-serial.js
@@ -1,4 +1,4 @@
-import {Group, sleep, version} from 'https://manulykebe.github.io/async-monitor.js/dist/async-monitor.esm.js';
+import {Group, Tree, sleep, version} from 'https://manulykebe.github.io/async-monitor.js/dist/async-monitor.esm.js';
 // import {Group, sleep, version} from 'http://127.0.0.1:5500/dist/async-monitor.esm.js';
 
 const serialWatches = new Group();
