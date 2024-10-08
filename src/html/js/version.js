e7d5089ce7b387cd1a2d3f00a40fe08128863af8
Manu Vanneste <125195959+manulykebe@users.noreply.github.com>
2024-10-08 17:45:41 +0200

    refrence to github distribution

--- src/html/js/version.js
+++ src/html/js/version.js
@@ -1,3 +1,3 @@
-// import {Group, sleep, version} from 'https://manulykebe.github.io/async-monitor.js/dist/async-monitor.esm.js';
-import {Group, Tree, sleep, version} from '/dist/async-monitor.esm.js';
+import {Group, sleep, version} from 'https://manulykebe.github.io/async-monitor.js/dist/async-monitor.esm.js';
+// import {Group, Tree, sleep, version} from '/dist/async-monitor.esm.js';
 console.log(`async-monitor.js@${version}`);
