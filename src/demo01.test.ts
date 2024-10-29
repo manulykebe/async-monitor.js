64f22caa830d49a9707be017ddf70f86f98d2362
Manu Vanneste <125195959+manulykebe@users.noreply.github.com>
2024-10-29 22:21:47 +0100

    updated test

--- src/demo01.test.ts
+++ src/demo01.test.ts
@@ -142,9 +142,92 @@ describe('addWatch: add a function', () => {
 		// Now run the watches and capture the metrics
 		await new Promise<void>((resolve, reject) => {
 			mixedWatches.useConsoleLog = false;
+			mixedWatches.onCompleteCallback = resolve;
 			mixedWatches.watchAll();
 		});
-
+		const expectedMetrics = [
+			{
+				id: 1,
+				name: 'watch-function-1',
+				start: 0,
+				duration: 1056,
+				status: 'fulfilled',
+				value: 1000,
+				reason: undefined,
+				isRunning: false,
+				isFinished: true,
+				isRejected: false,
+				isAborted: false,
+				sequence: 1,
+			},
+			{
+				id: 2,
+				name: 'watch-function-2',
+				start: 1062,
+				duration: 1005,
+				status: 'fulfilled',
+				value: 1000,
+				reason: undefined,
+				isRunning: false,
+				isFinished: true,
+				isRejected: false,
+				isAborted: false,
+				sequence: 2,
+			},
+			{
+				id: 3,
+				name: 'watch-function-3',
+				start: 2071,
+				duration: 1006,
+				status: 'fulfilled',
+				value: 1000,
+				reason: undefined,
+				isRunning: false,
+				isFinished: true,
+				isRejected: false,
+				isAborted: false,
+				sequence: 3,
+			},
+			{
+				id: 4,
+				name: 'watch-function-4',
+				start: 3081,
+				duration: 1012,
+				status: 'fulfilled',
+				value: 1000,
+				reason: undefined,
+				isRunning: false,
+				isFinished: true,
+				isRejected: false,
+				isAborted: false,
+				sequence: 4,
+			},
+			{
+				id: 5,
+				name: 'watch-function-5',
+				start: 4095,
+				duration: 1015,
+				status: 'fulfilled',
+				value: 1000,
+				reason: undefined,
+				isRunning: false,
+				isFinished: true,
+				isRejected: false,
+				isAborted: false,
+				sequence: 5,
+			},
+		];
 		// Check metrics after execution
-	});
+		mixedWatches.metrics.forEach((received, index) => {
+			const expected = expectedMetrics[index];
+			expect(received.id).toEqual(expected.id);
+			expect(received.name).toEqual(expected.name);
+			expect(received.status).toEqual(expected.status);
+			expect(received.value).toEqual(expected.value);
+
+			// Allowing some tolerance for timing-related values
+			expect(received.duration).toBeCloseTo(expected.duration, -2); // Tolerance of ±100
+			expect(received.start).toBeCloseTo(expected.start, -2); // Tolerance of ±100
+		});
+	}, 10000);
 });
